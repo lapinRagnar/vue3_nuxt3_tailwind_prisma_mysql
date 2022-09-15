c1c2e1417283f9321dc4b6c27e2d77f5bc215f54
lapinRagnar <lapinragnar@gmail.com>
2022-09-15 12:49:14 +0200

    2 - creation du template - sidebar left avec responsive

--- app.vue
+++ app.vue
@@ -2,6 +2,27 @@
   <div :class="{'dark': darkMode}">
     <h1 class="text-red-500 dark:text-dim-100">salut</h1>
   </div>
+
+  <div class="min-h-full">
+
+    <div class="grid grid-cols-12 mx-auto sm:px-6 lg:px-8 lg:gap-5">
+
+      <!-- sidebar gauche -->
+      <div class="hidden bg-yellow-300 md:block xs-col-span-1 xl:col-span-2 ">
+        <div class="sticky top-0">
+          <SidebarLeft/>
+        </div>
+      </div>
+
+      <!-- contenu principal -->
+
+      <!-- sidebar droite -->
+
+    </div>
+
+  </div>
+
+
 </template>
 
 <script setup>
---
+++ components/sidebar/Left.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    left sidebar
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
